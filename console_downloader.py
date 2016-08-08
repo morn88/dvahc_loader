5b149c998cbeb955a3ca469ca9bea74c2c473793
m0rn <morn88@gmail.com>
2016-08-08 10:56:00 +0300

    Initial commit

---
+++ console_downloader.py
@@ -0,0 +1,71 @@
+import urllib.parse
+import urllib.request
+import re
+import os
+import tqdm
+import sys
+
+
+def check_folder():
+    folder = 'done_webm'
+    try:
+        os.mkdir(folder)
+        print('Папка созданна')
+    except FileExistsError:
+        print('Папка готова')
+
+
+def connect(url):
+    try:
+        req = urllib.request.Request(url)
+        resp = urllib.request.urlopen(req)
+        respData = resp.read()
+
+        with open('links.txt', 'w') as saveFile:
+            saveFile.write(str(respData))
+        return 'OK'
+
+    except Exception as e:
+        print(str(e))
+        return str(e)
+
+
+def download(url):
+    url = url[0:-5].replace('res', 'src') + '/'
+    url_path = url.split('/')
+
+    with open('links.txt') as file:
+        string = file.read()
+
+    match = r'href="(.*?)"'
+    need = re.findall(match, string)
+    st_list = []
+    for st in need:
+        if st.endswith('.webm'):
+            st = st.replace('..', '/'.join(url_path[0:-3]))
+            st_list.append(st)
+
+    st_set = set(st_list)
+    file_list = os.listdir('./done_webm')
+    done_list = [url + i for i in file_list]
+
+    done_set = set(done_list)
+    downl_set = st_set.difference(done_set)
+    if len(downl_set) != 0:
+        return downl_set
+        os.chdir('./done_webm')
+        for i in tqdm.tqdm(downl_set):
+            fileName = i.split('/')
+            urllib.request.urlretrieve(i, fileName[-1])
+    else:
+        print('Нет новых файлов')
+if __name__ == '__main__':
+
+    check_folder()
+
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+        connect(url)
+        download(url)
+    else:
+        print('Укажите ссылку на тред')
---
+++ window.py
@@ -0,0 +1,105 @@
+from PyQt5 import QtGui, QtWidgets
+from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import QThread
+import sys
+import time
+import shutil
+import os
+from pyconacc import connect, download, check_folder
+import urllib
+
+
+class MyWind(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 100, 400, 200)
+        self.setWindowIcon(QtGui.QIcon('eba.png'))
+
+        self.tray_icon = SystemTrayIcon(QtGui.QIcon('eba.png'), self)
+        self.tray_icon.setToolTip('Готов к закачке')
+        self.tray_icon.show()
+
+        self.progress_bar = QtWidgets.QProgressBar(self)
+
+        timer = QTimer(self)
+        self.btn_copy = QtWidgets.QPushButton("Download")
+        self.btn_copy.clicked.connect(self.copy_files)
+
+        self.btn_connect = QtWidgets.QPushButton("Connect")
+        self.btn_connect.clicked.connect(self.connect_to_url)
+
+        self.label_out = QtWidgets.QLabel()
+        self.line_uri = QtWidgets.QLineEdit()
+
+        grid = QtWidgets.QGridLayout()
+        grid.addWidget(self.line_uri, 0, 0)
+        grid.addWidget(self.label_out, 1, 0)
+        grid.addWidget(self.progress_bar, 2, 0)
+        grid.addWidget(self.btn_connect, 3, 0)
+        grid.addWidget(self.btn_copy, 4, 0)
+
+        self.setLayout(grid)
+        self.show()
+
+    def connect_to_url(self):
+        try:
+            self.label_out.setText(connect(self.line_uri.text()))
+        except Exception as e:
+            self.label_out.setText(str(e))
+
+    def copy_files(self):
+        try:
+            check_folder()
+            downl_set = download(self.line_uri.text())
+            if downl_set is not None:
+                downl_set = list(downl_set)
+                step = 100 / len(downl_set)
+                value = step
+                count_of_files = len(downl_set)
+                self.label_out.setText('Файлов в папке: ' + str(count_of_files))
+                for i in range(len(downl_set)):
+                    urllib.request.urlretrieve(downl_set[i], os.path.join('done_webm',downl_set[i].split('/')[-1]))
+                    self.progress_bar.setValue(value)
+                    value += step
+                    count_of_files -= 1
+                    self.label_out.setText('Файлов: ' + str(count_of_files))
+                    self.tray_icon.setToolTip('Файлов: ' + str(count_of_files))
+
+            self.label_out.setText('None')
+            self.tray_icon.setToolTip('Ready')
+        except Exception as e:
+            print(str(e))
+            self.label_out.setText(str(e))
+
+    def update_time(self):
+        clock_time = time.ctime().split()[3]
+        self.clock.display(clock_time)
+
+
+class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
+
+    def __init__(self, icon, parent=None):
+        super(SystemTrayIcon, self).__init__(icon, parent)
+        menu = QtWidgets.QMenu(parent)
+        exitAction = menu.addAction("Выйти")
+        exitAction.triggered.connect(parent.close)
+        maximase_action = menu.addAction("Показать")
+        maximase_action.triggered.connect(self.size_change)
+        self.setContextMenu(menu)
+
+    def size_change(self):
+        if self.parent().isMaximized:
+            self.parent().showMinimized()
+        else:
+            self.parent().showMaximized()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    w = MyWind()
+    w.setWindowTitle('2ch downloader')
+    sys.exit(app.exec_())
